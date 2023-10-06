f129b71ebeba6aea3c2e2eed06be19b6432b8e26
nivinarayanan <145776077+nivinarayanan@users.noreply.github.com>
2023-10-06 20:47:19 +0530

    Delete Main(2).py

--- Main(2).py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-num = int(input("Enter a number: "))
-result = factorial(num)
-print("The factorial of", num, "is", result)
